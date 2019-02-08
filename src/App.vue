9dd8b3ddf83afe0d1a6e77f6d73f4641ce3758cb
Rolando Escalona <rolando.escalona@gmail.com>
2019-02-08 08:27:07 -0600

    Working on examples

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Grid :style="{height: '280px'}"
+          :data-items="items"
+          :columns="columns">
+    </Grid>
+
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { Button } from '@progress/kendo-buttons-vue-wrapper'
+import { Grid } from '@progress/kendo-vue-grid';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    Button,
+    Grid
+  },
+  data: function () {
+        return {
+            items: [],
+            columns: [
+                { field: 'ProductID'},
+                { field: 'ProductName', title: 'Product Name' },
+                { field: 'UnitPrice', title: 'Unit Price' }
+            ]
+        };
+    },
+    methods: {
+        createRandomData(count) {
+            const productNames = ['Chai', 'Chang', 'Syrup', 'Apple', 'Orange', 'Banana', 'Lemon', 'Pineapple', 'Tea', 'Milk'];
+            const unitPrices = [12.5, 10.1, 5.3, 7, 22.53, 16.22, 20, 50, 100, 120]
+
+            return Array(count).fill({}).map((_, idx) => ({
+                ProductID: idx + 1,
+                ProductName: productNames[Math.floor(Math.random() * productNames.length)],
+                UnitPrice: unitPrices[Math.floor(Math.random() * unitPrices.length)]
+            }));
+        }
+    },
+    mounted() {
+        this.items = this.createRandomData(50);
+    }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import '@progress/kendo-ui'
+import '@progress/kendo-theme-default/dist/all.css'
+import { Grid } from '@progress/kendo-vue-grid'
 Vue.config.productionTip = false
 
+import { ButtonsInstaller } from '@progress/kendo-buttons-vue-wrapper'
+ 
+Vue.use (ButtonsInstaller)
+Vue.component('Grid', Grid);
 new Vue({
   render: h => h(App),
 }).$mount('#app')
